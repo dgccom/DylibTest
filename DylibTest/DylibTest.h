ff5c92a2fe6cc1adcfa57d419773a0b75731548f
mac <mac@macdeMac-mini.local>
2015-08-24 14:57:14 +0800

    Initial Commit

---
+++ DylibTest/DylibTest.h
@@ -0,0 +1,19 @@
+//
+//  DylibTest.h
+//  DylibTest
+//
+//  Created by mac on 15/8/24.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DylibTest.
+FOUNDATION_EXPORT double DylibTestVersionNumber;
+
+//! Project version string for DylibTest.
+FOUNDATION_EXPORT const unsigned char DylibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DylibTest/PublicHeader.h>
+
+
